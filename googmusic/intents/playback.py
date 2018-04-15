ac4353f91a9e8cbf5c84e8e848c2e06e0eae1b19
WeCreate180 <tanayphone@gmail.com>
2018-04-14 20:59:54 -0400

    Sid

--- googmusic/intents/playback.py
+++ googmusic/intents/playback.py
@@ -27,17 +27,17 @@ def prev():
 
 @ask.intent("GoogMusicThumbsDown")
 def thumbsDown():
-    sid = music_queue.current()['nid']
+    sid = music_queue.current()['trackId']
     client.rate_songs(sid, 1)
 
 @ask.intent("GoogMusicThumbsUp")
 def thumbsUp():
-    sids = music_queue.current()['nid']
+    sids = music_queue.current()['trackId']
     client.rate_songs(sids, 5)
 
 @ask.intent("GoogMusicThumbsNone")
 def clearRating():
-    sidss = music_queue.current()['nid']
+    sidss = music_queue.current()['trackId']
     client.rate_songs(sidss, 0)
 
 @ask.on_playback_nearly_finished()
