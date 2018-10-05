77abba6be132571a0fea4fe4450aff18d62770a2
WeCreate180 <tanayphone@gmail.com>
2018-10-05 18:08:50 -0400

    Update server.py

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ import os
 ports = int(os.environ.get('PORT', 17995))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=ports, debug=True)
